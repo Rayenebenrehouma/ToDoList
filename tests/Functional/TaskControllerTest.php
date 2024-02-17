9c497c2a09c70cdc4bf8e33c6a54b2903fd72655
Ben Rehouma Rayen <rayenebenrehouma@outlook.fr>
2024-02-17 13:04:30 +0100

    ajout du reste de quelques test

--- tests/Functional/TaskControllerTest.php
+++ tests/Functional/TaskControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Functional;
 
+use App\Entity\Task;
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Request;
@@ -27,7 +28,7 @@ class TaskControllerTest extends WebTestCase
         $crawler = $client->request(Request::METHOD_GET, $urlGenerator->generate('task_create'));
 
         //Gérer un formulaire
-        $submitButton = $crawler->selectButton('Mettre à jour');
+        $submitButton = $crawler->selectButton('Créez une nouvelle tâche');
         $form = $submitButton->form();
         $form["task[title]"] = "PHPUNIT";
         $form["task[content]"] = "PHPUNIT CONTENT";
@@ -40,5 +41,69 @@ class TaskControllerTest extends WebTestCase
         $client->followRedirect();
         //Gérer l'alerte box et la route
         //$this->assertSelectorTextContains('div.alert-success', 'Votre task a été créer avec succès !');
+    }
+
+    public function testIfEditTaskIsSuccessfull(): void
+    {
+        $client = static::createClient();
+
+        $urlGenerator = $client->getContainer()->get('router');
+
+        $entityManager = $client->getContainer()->get('doctrine.orm.entity_manager');
+
+        $user = $entityManager->find(User::class, 10);
+        $task = $entityManager->getRepository(Task::class)->findOneBy([
+            'user' => $user
+        ]);
+
+        $client->loginUser($user);
+
+        $crawler = $client->request(
+            Request::METHOD_GET,
+            $urlGenerator->generate('task_edit', ['id' => $task->getId()])
+        );
+
+        $this->assertResponseIsSuccessful();
+
+        $submitButton = $crawler->selectButton('Mettre à jour');
+        $form = $submitButton->form();
+        $form["edit_task[title]"] = "NEW PHPUNIT";
+        $form["edit_task[content]"] = "NEW PHPUNIT CONTENT";
+        $form["edit_task[isDone]"] = false;
+
+        $client->submit($form);
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
+
+        $client->followRedirect();
+
+
+    }
+
+    public function testIfDeleteTaskIsSuccessfull(): void
+    {
+        $client = static::createClient();
+
+        $urlGenerator = $client->getContainer()->get('router');
+
+        $entityManager = $client->getContainer()->get('doctrine.orm.entity_manager');
+
+        $user = $entityManager->find(User::class, 10);
+        $task = $entityManager->getRepository(Task::class)->findOneBy([
+            'user' => $user
+        ]);
+
+        $client->loginUser($user);
+
+        //$id = 12;
+
+        $crawler = $client->request(
+            Request::METHOD_GET,
+            $urlGenerator->generate('task_delete', ['id' => $task->getId()])
+        );
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
+
+        $client->followRedirect();
     }
 }
